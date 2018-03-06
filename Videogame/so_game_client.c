95f761ede1b63767b2d9318db8f9e737d6f09a45
Krelian96 <francisfabbi96@gmail.com>
2018-03-06 19:18:25 +0100

    Krelian96

--- Videogame/so_game_client.c
+++ Videogame/so_game_client.c
@@ -10,11 +10,15 @@
 #include "world.h"
 #include "vehicle.h"
 #include "world_viewer.h"
+#include "so_game_protocol.h"
 
 int window;
 WorldViewer viewer;
 World world;
 Vehicle* vehicle; // The vehicle
+
+void* thread_listener(void* arg){}//todo
+
 
 void keyPressed(unsigned char key, int x, int y)
 {
@@ -82,7 +86,7 @@ void idle(void) {
   World_update(&world);
   usleep(30000);
   glutPostRedisplay();
-  
+
   // decay the commands
   vehicle->translational_force_update *= 0.999;
   vehicle->rotational_force_update *= 0.7;
@@ -101,7 +105,9 @@ int main(int argc, char **argv) {
   } else {
     printf("Fail! \n");
   }
-  
+
+  int ret;
+
   Image* my_texture_for_server;
   // todo: connect to the server
   //   -get ad id
@@ -109,11 +115,71 @@ int main(int argc, char **argv) {
   //   -get an elevation map
   //   -get the texture of the surface
 
+  //variables for handling a socket
+  int socket_desc;
+  struct sockaddr_in server_addr{0};    //some fields are required to be filled with 0
+
+  //creating a socket
+  socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_desc < 1) printf("Could not create socket");
+
+  //set up parameters for connection
+  server_addr.sin_addr.in_addr = inet_addr(SERVER_ADDRESS);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = SERVER_PORT;
+
+  //initiate a connection to the socket
+  ret = connect(socket_desc, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
+  if (ret < 0) printf("Could not connect to socket");
+
+  char idPacket[1024];
+  size_t idPacket_len = sizeof(idPacket);   //lunghezza del mex id del client
+  size_t msg_len;                           //lunghezza del messaggio letto
+
+  while ((ret = recv(socket_desc, idPacket, msg_len, 0)) < 0){
+    if (errno == EINTR) continue;
+    printf("Could not read id from socket");
+  }
+
+  idPacket[msg_len] = '\0';
+
+  IdPacket id = Packet_deserialize(idPacket, idPacket_len);
+
+  char elevation_map[1024];
+  size_t elevation_map_len = sizeof(elevation_map);
+
+  while ((ret = recv(socket_desc, elevation_map, msg_len, 0)) < 0){
+      if (errno == EINTR) continue;
+      printf("Could not read elevation map from socket");
+  }
+
+  elevation_map[msg_len] = '\0';
+
+  char texture_map[1024];
+  size_t texture_map_len = sizeof(texture_map);
+
+  while ((ret = recv(socket_desc, texture_map, msg_len, 0)) < 0){
+      if (errno == EINTR) continue;
+      printf("Could not read map texture from socket");
+  }
+
+  texture_map[msg_len] = '\0';
+
+  char my_texture[1024];
+  size_t my_texture_len = sizeof(my_texture);
+
+  while ((ret = recv(socket_desc, my_texture, msg_len, 0)) < 0){
+      if (errno == EINTR) continue;
+      printf("Could not read my texture from socket");
+  }
+
+  my_texture[msg_len] = '\0';
+
   // these come from the server
-  int my_id;
-  Image* map_elevation;
-  Image* map_texture;
-  Image* my_texture_from_server;
+  int my_id = id->id;
+  Image* map_elevation = Packet_deserialize(elevation_map, elevation_map_len);
+  Image* map_texture = Packet_deserialize(texture_map, texture_map_len);
+  Image* my_texture_from_server = Packet_deserialize(my_texture, my_texture_len);
 
   // construct the world
   World_init(&world, map_elevation, map_texture, 0.5, 0.5, 0.5);
@@ -129,9 +195,16 @@ int main(int argc, char **argv) {
   // request the texture and add the player to the pool
   /*FILLME*/
 
+  pthread_t thread;
+  ret = pthread_create(&thread, NULL, thread_listener, NULL);
+  if (ret < 0) printf("Could not create thread");
+
+  ret = pthread_join(&thread, NULL);
+  if (ret < 0) printf("Could not join thread");
+
   WorldViewer_runGlobal(&world, vehicle, &argc, argv);
 
   // cleanup
   World_destroy(&world);
-  return 0;             
+  return 0;
 }
